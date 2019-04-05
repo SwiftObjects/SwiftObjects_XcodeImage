3f70fbf45e7ceeffa0ae7e37e44dfe4953367177
Helge Heß <devteam@zeezide.de>
2019-04-05 17:43:28 +0200

    Use SO w/ proper runtime ref
    ...

--- Package.swift
+++ Package.swift
@@ -1,10 +1,6 @@
 // swift-tools-version:5.0
 import PackageDescription
 
-let runtimeLib: PackageDescription.Package.Dependency =
-                .package(url: "https://github.com/wickwirew/Runtime.git",
-                         .branch("swift5"))
-
 let package = Package(
     name: "SwiftObjects_XcodeImage",
 
@@ -18,12 +14,11 @@ let package = Package(
         .package(url: "https://github.com/onmyway133/SwiftHash.git",
                  from: "2.0.2"),
         .package(url: "https://github.com/SwiftWebResources/SemanticUI-Swift.git",
-                 from: "2.3.1"),
+                 from: "2.3.3"),
         .package(url: "https://github.com/SwiftWebResources/jQuery-Swift.git",
-                 from: "3.3.1"),
-        runtimeLib,
+                 from: "3.3.2"),
         .package(url: "https://github.com/SwiftObjects/SwiftObjects.git",
-                 from: "0.6.0")
+                 from: "0.6.1")
     ],
     targets: [
         .target(name: "SwiftObjects_XcodeImage", 
@@ -35,7 +30,6 @@ let package = Package(
                     "NIOFoundationCompat",
                     "NIOConcurrencyHelpers",
                     "SwiftHash",
-                    "Runtime",
                     "SwiftObjects",
                     "SemanticUI",
                     "jQuery"
