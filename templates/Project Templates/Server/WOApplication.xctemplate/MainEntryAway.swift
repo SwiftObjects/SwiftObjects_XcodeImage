4520fa1af4b52f834fbe559ca279d4565257174d
Helge Heß <me@helgehess.eu>
2019-04-11 18:48:30 +0200

    Build WOApp subclass via template
    ...

--- templates/Project Templates/Server/WOApplication.xctemplate/MainEntryAway.swift
+++
@@ -1,9 +0,0 @@
-//___FILEHEADER___
-
-import SwiftObjects
-
-// The code below starts the NIO HTTP server and runs our application.
-
-let WOApp  = Application()
-let server = WONIOAdaptor(application: WOApp)
-server.listenAndWait()
